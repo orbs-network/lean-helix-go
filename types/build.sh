4c614d80ad0a1faa0b964e535a3793571a3d673c
Ido Zilberberg <idoz@orbs.com>
2018-10-21 11:45:27 +0300

    build.sh fix (missing primitives directory)

--- types/build.sh
+++ types/build.sh
@@ -3,18 +3,20 @@
 echo "Make sure your membuffers git repo is updated and pulled before building!"
 echo ""
 
+mkdir -p ../primitives
+mkdir -p ./go
+
 #rm -rf lean_helix.mb.go
 #rm -rf ./go/*
 rm -f ./go/leanhelix/lean_helix.mb.go ./go/primitives/lean_helix_primitives.mb.go
 cp -r ../proto/* ./go
-
 
 ### OLD: (uses brew) membufc --go --mock `find . -name "*.proto"`
 ### NEW: running membufc directly from source to avoid waiting for brew releases
 echo "Proto files:"
 find . -name "*.proto"
 
-go run $(ls -1 ../../membuffers/go/membufc/*.go | grep -v _test.go) --go --mock `find . -name "*.proto"`
+go run $(ls -1 ../../membuffers/go/membufc/*.go | grep -v _test.go) --go --mock --go-ctx `find . -name "*.proto"`
 #rm `find . -name "*.proto"`
 rm -f ./go/leanhelix/lean_helix.proto ./go/primitives/lean_helix_primitives.proto
 mv ./go/leanhelix/lean_helix.mb.go ..
