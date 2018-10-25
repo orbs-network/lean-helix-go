3b8cb2c7173c999dcd21cfc67b573b4ffdb77eb5
Gil Amran <gil@orbs.com>
2018-10-25 18:06:59 +0300

    Remove the context (For now) from the register function

--- election_trigger.go
+++ election_trigger.go
@@ -1,11 +1,10 @@
 package leanhelix
 
 import (
-	"context"
 	"github.com/orbs-network/lean-helix-go/primitives"
 )
 
 type ElectionTrigger interface {
-	RegisterOnTrigger(view primitives.View, cb func(ctx context.Context, view primitives.View))
+	RegisterOnTrigger(view primitives.View, cb func(view primitives.View))
 	UnregisterOnTrigger()
 }
