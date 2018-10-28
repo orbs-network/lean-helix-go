81613195b63bacdab9cf4cb6ce06b2c49c1b5a64
Gil Amran <gil@orbs.com>
2018-10-28 16:44:07 +0200

    Renaming

--- test/message_factory_test.go
+++ test/message_factory_test.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"bytes"
-	"fmt"
 	lh "github.com/orbs-network/lean-helix-go"
 	. "github.com/orbs-network/lean-helix-go/primitives"
 	"github.com/orbs-network/lean-helix-go/test/builders"
@@ -13,105 +12,122 @@ import (
 )
 
 func TestMessageFactory(t *testing.T) {
-	leaderKeyManager := builders.NewMockKeyManager(Ed25519PublicKey("PK0"))
-	node1KeyManager := builders.NewMockKeyManager(Ed25519PublicKey("PK1"))
-	node2KeyManager := builders.NewMockKeyManager(Ed25519PublicKey("PK2"))
-	height := BlockHeight(math.Floor(rand.Float64() * 1000000))
+	keyManager := builders.NewMockKeyManager(Ed25519PublicKey("PK0"))
+	blockHeight := BlockHeight(math.Floor(rand.Float64() * 1000000))
 	view := View(math.Floor(rand.Float64() * 1000000))
 	block := builders.CreateBlock(builders.GenesisBlock)
 	blockHash := block.BlockHash()
-	leaderFac := lh.NewMessageFactory(leaderKeyManager)
+	node1KeyManager := builders.NewMockKeyManager(Ed25519PublicKey("PK1"))
+	node2KeyManager := builders.NewMockKeyManager(Ed25519PublicKey("PK2"))
+	leaderFac := lh.NewMessageFactory(keyManager)
 	node1Fac := lh.NewMessageFactory(node1KeyManager)
 	node2Fac := lh.NewMessageFactory(node2KeyManager)
 
 	t.Run("create PreprepareMessage", func(t *testing.T) {
 		signedHeader := &lh.BlockRefBuilder{
 			MessageType: lh.LEAN_HELIX_PREPREPARE,
-			BlockHeight: height,
+			BlockHeight: blockHeight,
 			View:        view,
 			BlockHash:   blockHash,
 		}
 		ppmcb := &lh.PreprepareContentBuilder{
 			SignedHeader: signedHeader,
 			Sender: &lh.SenderSignatureBuilder{
-				SenderPublicKey: leaderKeyManager.MyPublicKey(),
-				Signature:       leaderKeyManager.Sign(signedHeader.Build().Raw()),
+				SenderPublicKey: keyManager.MyPublicKey(),
+				Signature:       keyManager.Sign(signedHeader.Build().Raw()),
 			},
 		}
 
 		expectedPPM := lh.NewPreprepareMessage(ppmcb.Build(), block)
+		actualPPM := leaderFac.CreatePreprepareMessage(blockHeight, view, block)
 
-		actualPPM := leaderFac.CreatePreprepareMessage(height, view, block)
-		expectedPPMRaw := expectedPPM.Raw()
-		actualPPMRaw := actualPPM.Raw()
-
-		require.True(t, bytes.Compare(expectedPPMRaw, actualPPMRaw) == 0, "compared bytes of PPM")
+		require.True(t, bytes.Compare(expectedPPM.Raw(), actualPPM.Raw()) == 0, "compared bytes of PPM")
 	})
 
 	t.Run("create PrepareMessage", func(t *testing.T) {
 		signedHeader := &lh.BlockRefBuilder{
 			MessageType: lh.LEAN_HELIX_PREPARE,
-			BlockHeight: height,
+			BlockHeight: blockHeight,
 			View:        view,
 			BlockHash:   blockHash,
 		}
 		prepareContentBuilder := &lh.PrepareContentBuilder{
 			SignedHeader: signedHeader,
 			Sender: &lh.SenderSignatureBuilder{
-				SenderPublicKey: leaderKeyManager.MyPublicKey(),
-				Signature:       leaderKeyManager.Sign(signedHeader.Build().Raw()),
+				SenderPublicKey: keyManager.MyPublicKey(),
+				Signature:       keyManager.Sign(signedHeader.Build().Raw()),
 			},
 		}
+
 		expectedPM := lh.NewPrepareMessage(prepareContentBuilder.Build())
-		actualPM := leaderFac.CreatePrepareMessage(height, view, blockHash)
-		expectedPMRaw := expectedPM.Raw()
-		actualPMRaw := actualPM.Raw()
-		require.True(t, bytes.Compare(expectedPMRaw, actualPMRaw) == 0, "compared bytes of PM")
+		actualPM := leaderFac.CreatePrepareMessage(blockHeight, view, blockHash)
+
+		require.True(t, bytes.Compare(expectedPM.Raw(), actualPM.Raw()) == 0, "compared bytes of PM")
 	})
+
 	t.Run("create CommitMessage", func(t *testing.T) {
 		signedHeader := &lh.BlockRefBuilder{
 			MessageType: lh.LEAN_HELIX_COMMIT,
-			BlockHeight: height,
+			BlockHeight: blockHeight,
 			View:        view,
 			BlockHash:   blockHash,
 		}
 		cmcb := &lh.CommitContentBuilder{
 			SignedHeader: signedHeader,
 			Sender: &lh.SenderSignatureBuilder{
-				SenderPublicKey: leaderKeyManager.MyPublicKey(),
-				Signature:       leaderKeyManager.Sign(signedHeader.Build().Raw()),
+				SenderPublicKey: keyManager.MyPublicKey(),
+				Signature:       keyManager.Sign(signedHeader.Build().Raw()),
 			},
 		}
+
 		expectedCM := lh.NewCommitMessage(cmcb.Build())
-		actualCM := leaderFac.CreateCommitMessage(height, view, blockHash)
-		expectedCMRaw := expectedCM.Raw()
-		actualCMRaw := actualCM.Raw()
-		require.True(t, bytes.Compare(expectedCMRaw, actualCMRaw) == 0, "compared bytes of CM")
+		actualCM := leaderFac.CreateCommitMessage(blockHeight, view, blockHash)
+
+		require.True(t, bytes.Compare(expectedCM.Raw(), actualCM.Raw()) == 0, "compared bytes of CM")
 
 	})
 
-	// TODO This needs further testing - no proof, no pp or no p's with the proof
+	t.Run("create ViewChangeMessage without PreparedProof", func(t *testing.T) {
+		senderKeyManager := node1KeyManager
+		senderMessageFactory := node1Fac
+
+		signedHeader := &lh.ViewChangeHeaderBuilder{
+			MessageType:   lh.LEAN_HELIX_VIEW_CHANGE,
+			BlockHeight:   blockHeight,
+			View:          view,
+			PreparedProof: nil,
+		}
+		vcmContentBuilder := &lh.ViewChangeMessageContentBuilder{
+			SignedHeader: signedHeader,
+			Sender: &lh.SenderSignatureBuilder{
+				SenderPublicKey: senderKeyManager.MyPublicKey(),
+				Signature:       senderKeyManager.Sign(signedHeader.Build().Raw()),
+			},
+		}
+
+		actualVCM := senderMessageFactory.CreateViewChangeMessage(blockHeight, view, nil)
+		expectedVCM := lh.NewViewChangeMessage(vcmContentBuilder.Build(), nil)
+
+		require.True(t, bytes.Compare(expectedVCM.Raw(), actualVCM.Raw()) == 0, "compared bytes of VCM")
+	})
+
 	t.Run("create ViewChangeMessage with PreparedProof", func(t *testing.T) {
-
-		//proofBuilder := lh.CreatePreparedProofBuilder(leaderKeyManager, []lh.KeyManager{node1KeyManager, node2KeyManager}, height, view, blockHash)
-
-		// Decide which node is sending this message
 		senderKeyManager := node1KeyManager
 		senderMessageFactory := node1Fac
 
 		ppBlockRefBuilder := &lh.BlockRefBuilder{
 			MessageType: lh.LEAN_HELIX_PREPREPARE,
-			BlockHeight: height,
+			BlockHeight: blockHeight,
 			View:        view,
 			BlockHash:   blockHash,
 		}
 		ppSender := &lh.SenderSignatureBuilder{
-			SenderPublicKey: leaderKeyManager.MyPublicKey(),
-			Signature:       leaderKeyManager.Sign(ppBlockRefBuilder.Build().Raw()),
+			SenderPublicKey: keyManager.MyPublicKey(),
+			Signature:       keyManager.Sign(ppBlockRefBuilder.Build().Raw()),
 		}
 		pBlockRefBuilder := &lh.BlockRefBuilder{
 			MessageType: lh.LEAN_HELIX_PREPARE,
-			BlockHeight: height,
+			BlockHeight: blockHeight,
 			View:        view,
 			BlockHash:   blockHash,
 		}
@@ -133,7 +149,7 @@ func TestMessageFactory(t *testing.T) {
 		}
 		signedHeader := &lh.ViewChangeHeaderBuilder{
 			MessageType:   lh.LEAN_HELIX_VIEW_CHANGE,
-			BlockHeight:   height,
+			BlockHeight:   blockHeight,
 			View:          view,
 			PreparedProof: proofBuilder,
 		}
@@ -145,47 +161,37 @@ func TestMessageFactory(t *testing.T) {
 			},
 		}
 
-		expectedVCM := lh.NewViewChangeMessage(vcmContentBuilder.Build(), block)
 		preparedMessages := &lh.PreparedMessages{
-			PreprepareMessage: leaderFac.CreatePreprepareMessage(height, view, block),
+			PreprepareMessage: leaderFac.CreatePreprepareMessage(blockHeight, view, block),
 			PrepareMessages: []*lh.PrepareMessage{
-				node1Fac.CreatePrepareMessage(height, view, blockHash),
-				node2Fac.CreatePrepareMessage(height, view, blockHash),
+				node1Fac.CreatePrepareMessage(blockHeight, view, blockHash),
+				node2Fac.CreatePrepareMessage(blockHeight, view, blockHash),
 			},
 		}
-		actualVCM := senderMessageFactory.CreateViewChangeMessage(height, view, preparedMessages)
-		fmt.Println("Expected:", expectedVCM.String())
-		fmt.Println("Actual:  ", actualVCM.String())
-		expectedVCMRaw := expectedVCM.Raw()
-		actualVCMRaw := actualVCM.Raw()
-		require.True(t, bytes.Compare(expectedVCMRaw, actualVCMRaw) == 0, "compared bytes of VCM")
 
-	})
+		actualVCM := senderMessageFactory.CreateViewChangeMessage(blockHeight, view, preparedMessages)
+		expectedVCM := lh.NewViewChangeMessage(vcmContentBuilder.Build(), block)
 
-	t.Run("create ViewChangeMessage without PreparedProof", func(t *testing.T) {
-		t.Skip()
+		require.True(t, bytes.Compare(expectedVCM.Raw(), actualVCM.Raw()) == 0, "compared bytes of VCM")
 	})
 
 	t.Run("create NewViewMessage", func(t *testing.T) {
-
-		// This test passes falsely!!!
-
 		// This test assumes all non-leader nodes hold PreparedProof and also that it is the same for all of them
 
 		// Construct the "expected" message manually
 		ppBlockRefBuilder := &lh.BlockRefBuilder{
 			MessageType: lh.LEAN_HELIX_PREPREPARE,
-			BlockHeight: height,
+			BlockHeight: blockHeight,
 			View:        view,
 			BlockHash:   blockHash,
 		}
 		ppSender := &lh.SenderSignatureBuilder{
-			SenderPublicKey: leaderKeyManager.MyPublicKey(),
-			Signature:       leaderKeyManager.Sign(ppBlockRefBuilder.Build().Raw()),
+			SenderPublicKey: keyManager.MyPublicKey(),
+			Signature:       keyManager.Sign(ppBlockRefBuilder.Build().Raw()),
 		}
 		pBlockRefBuilder := &lh.BlockRefBuilder{
 			MessageType: lh.LEAN_HELIX_PREPARE,
-			BlockHeight: height,
+			BlockHeight: blockHeight,
 			View:        view,
 			BlockHash:   blockHash,
 		}
@@ -208,7 +214,7 @@ func TestMessageFactory(t *testing.T) {
 
 		nodesVCHeader := &lh.ViewChangeHeaderBuilder{
 			MessageType:   lh.LEAN_HELIX_VIEW_CHANGE,
-			BlockHeight:   height,
+			BlockHeight:   blockHeight,
 			View:          view,
 			PreparedProof: proofBuilder,
 		}
@@ -228,15 +234,15 @@ func TestMessageFactory(t *testing.T) {
 		}
 		nvmHeader := &lh.NewViewHeaderBuilder{
 			MessageType: lh.LEAN_HELIX_NEW_VIEW,
-			BlockHeight: height,
+			BlockHeight: blockHeight,
 			View:        view,
 			ViewChangeConfirmations: []*lh.ViewChangeMessageContentBuilder{
 				node1Confirmation, node2Confirmation,
 			},
 		}
 		nvmSender := &lh.SenderSignatureBuilder{
-			SenderPublicKey: leaderKeyManager.MyPublicKey(),
-			Signature:       leaderKeyManager.Sign(nvmHeader.Build().Raw()),
+			SenderPublicKey: keyManager.MyPublicKey(),
+			Signature:       keyManager.Sign(nvmHeader.Build().Raw()),
 		}
 		nvmContentBuilder := &lh.NewViewMessageContentBuilder{
 			SignedHeader: nvmHeader,
@@ -247,36 +253,30 @@ func TestMessageFactory(t *testing.T) {
 			},
 		}
 
-		expectedNVM := lh.NewNewViewMessage(nvmContentBuilder.Build(), block)
-
 		// Construct "actual" message with message factories
-		ppm := leaderFac.CreatePreprepareMessage(height, view, block)
+		ppm := leaderFac.CreatePreprepareMessage(blockHeight, view, block)
 		preparedMessages := &lh.PreparedMessages{
 			PreprepareMessage: ppm,
 			PrepareMessages: []*lh.PrepareMessage{
-				node1Fac.CreatePrepareMessage(height, view, blockHash),
-				node2Fac.CreatePrepareMessage(height, view, blockHash),
+				node1Fac.CreatePrepareMessage(blockHeight, view, blockHash),
+				node2Fac.CreatePrepareMessage(blockHeight, view, blockHash),
 			},
 		}
 		confirmations := []*lh.ViewChangeMessageContentBuilder{
-			node1Fac.CreateViewChangeMessageContentBuilder(height, view, preparedMessages),
-			node2Fac.CreateViewChangeMessageContentBuilder(height, view, preparedMessages),
+			node1Fac.CreateViewChangeMessageContentBuilder(blockHeight, view, preparedMessages),
+			node2Fac.CreateViewChangeMessageContentBuilder(blockHeight, view, preparedMessages),
 		}
+
 		actualNVM := leaderFac.CreateNewViewMessage(
-			height,
+			blockHeight,
 			view,
-			leaderFac.CreatePreprepareMessageContentBuilder(height, view, block),
+			leaderFac.CreatePreprepareMessageContentBuilder(blockHeight, view, block),
 			confirmations,
 			block)
+		expectedNVM := lh.NewNewViewMessage(nvmContentBuilder.Build(), block)
 
-		fmt.Println(expectedNVM)
-		fmt.Println(actualNVM)
-		expectedNVMRaw := expectedNVM.Raw()
-		actualNVMRaw := actualNVM.Raw()
-		require.True(t, bytes.Compare(expectedNVMRaw, actualNVMRaw) == 0, "compared bytes of NVM")
+		require.True(t, bytes.Compare(expectedNVM.Raw(), actualNVM.Raw()) == 0, "compared bytes of NVM")
 
 	})
 
 }
-
-// TODO VCM & NVM from MessageFactory.spec.ts
