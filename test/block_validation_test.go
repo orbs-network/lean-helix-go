34ae9c8f2eae09697e225561999c3bc02a36515f
Gil Amran <gil@orbs.com>
2018-11-11 19:04:06 +0200

    Testing no consensus because of failing block validation

--- test/block_validation_test.go
+++ test/block_validation_test.go
@@ -23,3 +23,17 @@ func TestHappyFlow(t *testing.T) {
 
 	require.True(t, testNetwork.AllNodesAgreeOnBlock(block1))
 }
+
+func TestNoConsensusWhenValidationFailed(t *testing.T) {
+	block1 := builders.CreateBlock(builders.GenesisBlock)
+	block2 := builders.CreateBlock(block1)
+
+	testNetwork := builders.ATestNetwork(4, []leanhelix.Block{block1, block2})
+	testNetwork.Nodes[0].BlockUtils.FailValidations()
+	testNetwork.Nodes[1].BlockUtils.FailValidations()
+	testNetwork.Nodes[2].BlockUtils.FailValidations()
+	testNetwork.Nodes[3].BlockUtils.FailValidations()
+	testNetwork.StartConsensusOnAllNodes()
+
+	require.False(t, testNetwork.AllNodesAgreeOnBlock(block1))
+}
