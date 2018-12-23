f82e29145a8cf50c291f814baf5ffe19e3460218
Gil Amran <gil@orbs.com>
2018-12-23 11:31:43 +0200

    Testing GeneratingBlockProof

--- test/blockproof_test.go
+++ test/blockproof_test.go
@@ -5,8 +5,14 @@ import (
 	"github.com/orbs-network/lean-helix-go/spec/types/go/primitives"
 	"github.com/orbs-network/lean-helix-go/spec/types/go/protocol"
 	"github.com/orbs-network/lean-helix-go/test/builders"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
+
+func CompareSenderSignature(commitMessage *leanhelix.CommitMessage, senderSignature *protocol.SenderSignature) bool {
+	sender := commitMessage.Content().Sender()
+	return sender.Signature().Equal(senderSignature.Signature()) && sender.MemberId().Equal(senderSignature.MemberId())
+}
 
 func TestGeneratingBlockProof(t *testing.T) {
 	block := builders.CreateBlock(leanhelix.GenesisBlock)
@@ -21,16 +27,26 @@ func TestGeneratingBlockProof(t *testing.T) {
 	node2KeyManager := builders.NewMockKeyManager(memberId2)
 	node3KeyManager := builders.NewMockKeyManager(memberId3)
 
-	cm0 := builders.ACommitMessage(node1KeyManager, memberId1, 1, 1, block)
-	cm1 := builders.ACommitMessage(node2KeyManager, memberId2, 1, 1, block)
-	cm2 := builders.ACommitMessage(node3KeyManager, memberId3, 1, 1, block)
-	cm3 := builders.ACommitMessage(node0KeyManager, memberId0, 1, 1, block)
+	cm0 := builders.ACommitMessage(node1KeyManager, memberId1, 5, 6, block)
+	cm1 := builders.ACommitMessage(node2KeyManager, memberId2, 5, 6, block)
+	cm2 := builders.ACommitMessage(node3KeyManager, memberId3, 5, 6, block)
+	cm3 := builders.ACommitMessage(node0KeyManager, memberId0, 5, 6, block)
 
 	commitMessages := []*leanhelix.CommitMessage{cm0, cm1, cm2, cm3}
 
-	aggregateFunc := func(blockHeight primitives.BlockHeight, randomSeedShares []*protocol.SenderSignature) primitives.RandomSeedSignature {
-		return primitives.RandomSeedSignature("ResultRandomSeedSignature")
-	}
+	blockProof := leanhelix.GenerateLeanHelixBlockProof(commitMessages)
 
-	leanhelix.GenerateLeanHelixBlockProof(commitMessages, aggregateFunc)
+	// BlockRef
+	blockRef := blockProof.BlockRef()
+	require.Equal(t, protocol.LEAN_HELIX_COMMIT, blockRef.MessageType())
+	require.Equal(t, primitives.BlockHeight(5), blockRef.BlockHeight())
+	require.Equal(t, primitives.View(6), blockRef.View())
+	require.True(t, builders.CalculateBlockHash(block).Equal(blockRef.BlockHash()))
+
+	i := blockProof.NodesIterator()
+	require.True(t, CompareSenderSignature(cm0, i.NextNodes()))
+	require.True(t, CompareSenderSignature(cm1, i.NextNodes()))
+	require.True(t, CompareSenderSignature(cm2, i.NextNodes()))
+	require.True(t, CompareSenderSignature(cm3, i.NextNodes()))
+	require.False(t, i.HasNext())
 }
