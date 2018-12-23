fbdf2f7b92d154afbe2cff58457b131e4dd9d2db
Gil Amran <gil@orbs.com>
2018-12-23 11:32:20 +0200

    Removed the RandomSeedSignature from the BlockProof (For now)

--- blockProof.go
+++ blockProof.go
@@ -1,15 +1,11 @@
 package leanhelix
 
 import (
-	"github.com/orbs-network/lean-helix-go/spec/types/go/primitives"
 	"github.com/orbs-network/lean-helix-go/spec/types/go/protocol"
 )
 
-type AggregateRandomSeed func(blockHeight primitives.BlockHeight, randomSeedShares []*protocol.SenderSignature) primitives.RandomSeedSignature
-
 // assume commit messages are valid and still hold
-func GenerateLeanHelixBlockProof(commitMessages []*CommitMessage, aggregateRandomSeed AggregateRandomSeed) *protocol.BlockProof {
-
+func GenerateLeanHelixBlockProof(commitMessages []*CommitMessage) *protocol.BlockProof {
 	blockHeight := commitMessages[0].BlockHeight()
 	blockRefBuilder := &protocol.BlockRefBuilder{
 		MessageType: protocol.LEAN_HELIX_COMMIT,
@@ -19,25 +15,16 @@ func GenerateLeanHelixBlockProof(commitMessages []*CommitMessage, aggregateRando
 	}
 
 	cSendersBuilders := make([]*protocol.SenderSignatureBuilder, 0)
-	cShares := make([]*protocol.SenderSignature, 0)
 	for _, cm := range commitMessages {
 		memberId := cm.Content().Sender().MemberId()
 		cSendersBuilders = append(cSendersBuilders, &protocol.SenderSignatureBuilder{
 			MemberId:  memberId,
 			Signature: cm.Content().Sender().Signature(),
 		})
-
-		cShares = append(cShares, (&protocol.SenderSignatureBuilder{
-			MemberId:  memberId,
-			Signature: primitives.Signature(cm.Content().Share()), //Variable length
-		}).Build())
-
 	}
 
-	randomSeedSignature := aggregateRandomSeed(blockHeight, cShares)
 	return (&protocol.BlockProofBuilder{
-		BlockRef:            blockRefBuilder,
-		Nodes:               cSendersBuilders,
-		RandomSeedSignature: randomSeedSignature,
+		BlockRef: blockRefBuilder,
+		Nodes:    cSendersBuilders,
 	}).Build()
 }
