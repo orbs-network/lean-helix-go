8e8f937302021b0ecd2efca88d7b44af4c337176
Ido Zilberberg <idoz@orbs.com>
2019-02-11 10:41:18 +0200

    fmt

--- services/electiontrigger/test/stress_test.go
+++ services/electiontrigger/test/stress_test.go
@@ -2,19 +2,19 @@ package test
 
 import (
 	"context"
-	"github.com/orbs-network/lean-helix-go/test"
 	"github.com/orbs-network/lean-helix-go/spec/types/go/primitives"
+	"github.com/orbs-network/lean-helix-go/test"
 	"testing"
 	"time"
-	)
+)
 
 func TestStress_FrequentRegisters(t *testing.T) {
 	test.WithContext(func(ctx context.Context) {
-		et  := buildElectionTrigger(ctx, 1*time.Microsecond)
+		et := buildElectionTrigger(ctx, 1*time.Microsecond)
 
-		for h:=primitives.BlockHeight(1); h<primitives.BlockHeight(1000); h++ {
+		for h := primitives.BlockHeight(1); h < primitives.BlockHeight(1000); h++ {
 			et.RegisterOnElection(ctx, h, 0, nil)
-			time.Sleep(1*time.Microsecond)
+			time.Sleep(1 * time.Microsecond)
 		}
 	})
 }
