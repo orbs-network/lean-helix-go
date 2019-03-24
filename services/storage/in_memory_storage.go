5bfa9c524092c83628b4eaf7d1fc0352c74741af
Tal Kol <talkol@gmail.com>
2019-03-24 13:52:56 +0200

    Added license files

--- services/storage/in_memory_storage.go
+++ services/storage/in_memory_storage.go
@@ -1,3 +1,9 @@
+// Copyright 2019 the lean-helix-go authors
+// This file is part of the lean-helix-go library in the Orbs project.
+//
+// This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
+// The above notice should be included in all copies or substantial portions of the software.
+
 package storage
 
 import (
