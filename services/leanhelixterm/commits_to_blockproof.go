3818178c01aee38024300a3d7ae37511bbb28889
Ido Zilberberg <idoz@orbs.com>
2019-04-16 15:47:42 +0300

    Minor renaming

--- services/leanhelixterm/commits_to_blockproof.go
+++ services/leanhelixterm/commits_to_blockproof.go
@@ -21,16 +21,17 @@ import (
 func CommitsToProof(log logger.LHLogger, blockHeight primitives.BlockHeight, myMemberId primitives.MemberId, keyManager interfaces.KeyManager, onCommit interfaces.OnCommitCallback) termincommittee.OnInCommitteeCommitCallback {
 	return func(ctx context.Context, block interfaces.Block, commitMessages []*interfaces.CommitMessage) {
 		proof := blockproof.GenerateLeanHelixBlockProof(keyManager, commitMessages)
-		committeeStr := commitMessagesToMemberIdsStr(commitMessages)
+		committeeStr := commitMessagesToCommitteeMemberIdsStr(commitMessages)
 		log.Info(L.LC(blockHeight, math.MaxUint64, myMemberId), "Generated block proof with committee-size=%d, committee-members=%s", len(commitMessages), committeeStr)
 		onCommit(ctx, block, proof.Raw())
 	}
 }
 
-func commitMessagesToMemberIdsStr(messages []*interfaces.CommitMessage) string {
-	ids := make([]string, 0)
+func commitMessagesToCommitteeMemberIdsStr(messages []*interfaces.CommitMessage) string {
+	committeeMemberIds := make([]string, 0)
 	for _, cm := range messages {
-		ids = append(ids, cm.Content().Sender().MemberId().String())
+		committeeMemberId := cm.Content().Sender().MemberId().String()
+		committeeMemberIds = append(committeeMemberIds, committeeMemberId)
 	}
-	return strings.Join(ids, ",")
+	return strings.Join(committeeMemberIds, ",")
 }
