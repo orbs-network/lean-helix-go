f3f198a9d96c1791923860307ab25fd6b4defcb0
Ron <ron@orbs.com>
2019-08-03 15:42:53 +0300

    main and worker loops GoForever. naive scribe intergration

--- mainloop.go
+++ mainloop.go
@@ -3,6 +3,7 @@ package leanhelix
 import (
 	"context"
 	"fmt"
+	"github.com/orbs-network/govnr"
 	"github.com/orbs-network/lean-helix-go/services/electiontrigger"
 	"github.com/orbs-network/lean-helix-go/services/interfaces"
 	L "github.com/orbs-network/lean-helix-go/services/logger"
@@ -10,6 +11,7 @@ import (
 	"github.com/orbs-network/lean-helix-go/spec/types/go/primitives"
 	"github.com/orbs-network/lean-helix-go/spec/types/go/protocol"
 	"github.com/orbs-network/lean-helix-go/state"
+	"github.com/orbs-network/scribe/log"
 	"github.com/pkg/errors"
 )
 
@@ -52,14 +54,10 @@ func NewLeanHelix(config *interfaces.Config, onCommitCallback interfaces.OnCommi
 // ORBS: LeanHelix.Run(ctx, goroutineLauncher func(f func()) { GoForever(f) }))
 // LH: goroutineLauncher(func (){m.runWorkerLoop(ctx)})
 
-func (m *MainLoop) Run(ctx context.Context) <-chan struct{} {
-	done := make(chan struct{})
-	// TODO Replace wih supervised.GoForever()
-	go func() {
+func (m *MainLoop) Run(ctx context.Context) govnr.ContextEndedChan {
+	return govnr.GoForever(ctx, log.GetLogger().WithTags(log.Node(m.config.InstanceId.String()), log.String("event_loop", "LHMain")), func() {
 		m.run(ctx)
-		close(done)
-	}()
-	return done
+	})
 }
 
 func (m *MainLoop) runWorkerLoop(ctx context.Context) {
@@ -71,8 +69,9 @@ func (m *MainLoop) runWorkerLoop(ctx context.Context) {
 		m.onCommitCallback,
 		m.onNewConsensusRoundCallback)
 
-	// TODO Replace wih supervised.GoForever()
-	go m.worker.Run(ctx)
+	govnr.GoForever(ctx, log.GetLogger().WithTags(log.Node(m.config.InstanceId.String()), log.String("event_loop", "LHWorker")), func() {
+	 	m.worker.Run(ctx)
+	})
 }
 
 func (m *MainLoop) run(ctx context.Context) {
